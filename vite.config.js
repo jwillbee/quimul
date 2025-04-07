a9c5864334868f8a9858f4b4b90134ff3b99a5a6
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-04-07 09:59:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  }
+    outDir: 'dist', 
+    assetsDir: 'assets', 
+    rollupOptions: {
+      input: './index.html' 
+    }
+  },
+  base: '/',
 });
-
