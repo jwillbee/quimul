f0ced7628ff90e92f2116e7d5fd36dddd68e287a
jwillbee <33378604+jwillbee@users.noreply.github.com>
2025-07-12 14:49:26 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import NumberPad from './components/NumberPad';
 import ResultsCard from './components/ResultsCard';
-import { fetchDailyProblems } from './api/problems';
+import { fetchDailyProblems } from './api';
 
 const App = () => {
   const [problems, setProblems] = useState([]);
